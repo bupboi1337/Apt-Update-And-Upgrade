01f89dd1ac6434a6870e7657b7a637c3e0d0aa02
JymPatel <jympatel@yahoo.com>
2022-05-10 23:47:08 +0530

    things are now coloured

--- APTupdater.bash
+++ APTupdater.bash
@@ -1,10 +1,17 @@
+#!/bin/bash
 #
 # APT Updater 1.0
 #
-echo "Updating..."
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
+echo -e "${YELLOW}Updating...${RESET}"
 sudo apt-get update
-echo "Update done!"
-echo "Upgrading..."
+echo -e "${GREEN}Update done!${RESET}"
+echo -e "${YELLOW}Upgrading...${RESET}"
 sudo apt upgrade
-echo "Upgrade Done!"
-echo "Done!"
+echo -e "${GREEN}Upgrade Done!${RESET}"
+echo -e "${BLUE}Done updating and upgrading existing packages!${RESET}"
